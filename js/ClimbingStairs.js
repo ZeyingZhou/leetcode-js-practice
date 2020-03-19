0f44bbc4224485287ade7ae19f277897f7deaa68
ZeyingZhou <47362333+ZeyingZhou@users.noreply.github.com>
2020-03-19 13:20:29 -0400

    update

---
+++ js/ClimbingStairs.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+
+let climbStairs = (n) => {
+    if(n===1){
+        return 1
+    }
+    let first = 1;
+    let second = 2;
+    for(let i = 3; i <= n; i++){
+        let third = first + second;
+        first = second;
+        second = third;
+    }
+    return second;
+}
+
+climbStairs(5);
